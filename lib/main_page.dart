a59d076f30cad82df1dec0a9c15a3c1dfb34460b
Bagus Wicaksono <baguswicaksono388@gmail.com>
2020-09-03 16:00:27 +0700

    Dev [Bagus] Menambahkan tombol POST pada MainPage

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,10 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:form_login/auth_services.dart';
+import 'auth_services.dart';
+import 'model/post_model_result.dart';
 
-class MainPage extends StatelessWidget {
-  final FirebaseUser user;
-  MainPage(this.user);
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() {
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  PostResult postResult;
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -12,8 +21,37 @@ class MainPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(user.uid),
+            // Text(),
+            FutureBuilder(
+              future: FirebaseAuth.instance.currentUser(),
+              builder: (context, AsyncSnapshot<FirebaseUser> snapshot) {
+                if (snapshot.hasData) {
+                  return Text(snapshot.data.email);
+                } else {
+                  return Text('Loading...');
+                }
+              },
+            ),
+            Text((postResult != null)
+                ? postResult.id +
+                    " | " +
+                    postResult.name +
+                    " | " +
+                    postResult.job +
+                    " | " +
+                    postResult.created
+                : "Tidak ada data"),
+            RaisedButton(
+              onPressed: () {
+                PostResult.connectToAPI("Tejo", "TNI").then((value) {
+                  postResult = value;
+                  setState(() {});
+                });
+              },
+              child: Text("POST"),
+            ),
             RaisedButton(
                 child: Text("Sign Out"),
                 onPressed: () async {
